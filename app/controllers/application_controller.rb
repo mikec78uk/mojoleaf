e37009862051774bf404623bf37d79a86a080056
Mike Clarke <clarke.michael.j@gmail.com>
2014-04-26 10:21:50 +0100

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   protected
-  # Redirects to holiday path after sign in (inc. protected part above)
+ 
   def after_sign_in_path_for(resource)
     tests_path
   end
