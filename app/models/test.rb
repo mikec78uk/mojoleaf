d7169e52a4acd69fa6d2d1795b2a3cc329d8b53a
Mike Clarke <clarke.michael.j@gmail.com>
2015-03-12 20:36:29 +0000

    another attempt

--- app/models/test.rb
+++ app/models/test.rb
@@ -79,11 +79,10 @@ class Test < ActiveRecord::Base
 
 		# Checks that http or https is present in the chosen_destination and adds it if not
 		
-		unless @current_tests.blank?
-			unless self.chosen_destination[/\Ahttp:\/\//] || self.chosen_destination[/\Ahttps:\/\//]
-		    	self.chosen_destination = "http://#{self.chosen_destination}"
-		 	end
-		end
+		unless self.chosen_destination[/\Ahttp:\/\//] || self.chosen_destination[/\Ahttps:\/\//]
+	    	self.chosen_destination = "http://#{self.chosen_destination}"
+	 	end
+		
 
 	end
 		
